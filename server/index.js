7f1e8aec3fad6292acc46ece4bf63ef144c581e4
Kushal Maru <kushalmaru28@gmail.com>
2024-06-26 16:22:24 +0530

    http server, methods and express

---
+++ server/index.js
@@ -0,0 +1,45 @@
+// const http = require("http")
+// const fs = require("fs")
+// const url = require("url")
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send("Hello from home page")
+});                                             // Using express
+
+app.get('/about', (req, res) => {
+    return res.send("Hello from about page" + ' hey ' + req.query.name + ' you are ' + req.query.age)
+})                                             // Using express
+
+
+
+app.listen(8000, () => console.log("server started"))
+
+// function myHandler(req, res){
+//     if(req.url === "/favicon.ico") return res.end()
+//         const log = `${Date.now()}: ${req.url} New Req Recieved\n`
+//         const myUrl = url.parse(req.url, true)
+//         console.log(myUrl);
+//         fs.appendFile("log.txt", log, (err, data) => {
+//             switch (myUrl.pathname) {
+//                 case '/': res.end("HomePage")
+//                     break;
+    
+//                 case '/about': 
+//                 const username = myUrl.query.myname;
+//                 res.end(`Hi, ${username}`)
+//                     break;
+            
+//                 default: res.end("404 Not Found")
+//                     break;
+//             }
+            
+//         })
+// }                                             // Using normally
+
+
+// const myServer = http.createServer(app);
+
+// myServer.listen(8000, () => console.log("server started"))
